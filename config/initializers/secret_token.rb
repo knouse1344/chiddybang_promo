09c87aff3dff893fe8b371c28935f73cafd499a0
Bryan <bryan.knouse@gmail.com>
2014-09-18 10:27:49 -0400

    first commit!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ChiddyBang::Application.config.secret_key_base = '34f515ce963da93afcd8ff5a84010a22e3f7711cdc895ec2e116b7e7b666f54508a6148fac50fd8d6dee876613949bf940f4f8438fc73cb1483564da4bab7ad5'
